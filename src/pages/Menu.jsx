a5c56f0d4d1f445c064ed0d4a36b4e844ced179c
divakar-scode <67458356+divakar-scode@users.noreply.github.com>
2026-07-15 23:41:30 +0530

    Update Menu component to use shared cart state

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,24 +1,10 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import foodData from '../data/foodData.json';
 import FoodCard from '../components/FoodCard';
 import './Menu.css';
 
-function Menu() {
-  const [cart, setCart] = useState([]);
-
+function Menu({ cart, onAddToCart }) {
   const memoizedFoodData = useMemo(() => foodData, []);
-
-  const handleAddToCart = (food) => {
-    setCart((prevCart) => {
-      const existingItem = prevCart.find((item) => item.id === food.id);
-      if (existingItem) {
-        return prevCart.map((item) =>
-          item.id === food.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      }
-      return [...prevCart, { ...food, quantity: 1 }];
-    });
-  };
 
   return (
     <div className="menu-container">
@@ -28,7 +14,7 @@ function Menu() {
           <FoodCard
             key={food.id}
             food={food}
-            onAddToCart={handleAddToCart}
+            onAddToCart={onAddToCart}
           />
         ))}
       </div>
