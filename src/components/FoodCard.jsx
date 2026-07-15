08dea1e7ac53c5892ea34b7f455ef5624973d359
divakar-scode <67458356+divakar-scode@users.noreply.github.com>
2026-07-15 23:38:46 +0530

    Add alert message when item is added to cart

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './FoodCard.css';
 
 function FoodCard({ food, onAddToCart }) {
+  const handleClick = () => {
+    onAddToCart(food);
+    // Show alert when item is added to cart
+    alert(`${food.name} has been added to your cart!`);
+  };
+
   return (
     <div className="food-card">
       <img src={food.image} alt={food.name} className="food-image" />
@@ -9,7 +15,7 @@ function FoodCard({ food, onAddToCart }) {
         <h3>{food.name}</h3>
         <p className="description">{food.description}</p>
         <p className="price">₹{food.price.toFixed(2)}</p>
-        <button className="add-to-cart-btn" onClick={() => onAddToCart(food)}>
+        <button className="add-to-cart-btn" onClick={handleClick}>
           Add to Cart
         </button>
       </div>
